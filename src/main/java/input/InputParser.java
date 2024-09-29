4bc9cb71db3ba77e1e1fe3fbc38705932b8a9450
Jez <jezdavisuk@gmail.com>
2024-09-29 17:48:13 +0100

    feat: add instance variable of type Queue holds validatedInstructions

--- src/main/java/input/InputParser.java
+++ src/main/java/input/InputParser.java
@@ -1,6 +1,19 @@
 package input;
 
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputParser {
+
+    private final Queue<Instruction> validatedInstructions = new LinkedList<>();
+
+    public Queue<Instruction> getValidatedInstructions() {
+        return validatedInstructions;
+    }
+
 
 
 }
