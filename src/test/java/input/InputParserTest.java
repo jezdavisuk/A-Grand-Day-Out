9a4cd200cda9f5ed8be29758611051fe5326053b
Jez <jezdavisuk@gmail.com>
2024-09-29 18:35:24 +0100

    feat: add method testParsesMultipleValidInstructions() passes on class InputParserTest

--- src/test/java/input/InputParserTest.java
+++ src/test/java/input/InputParserTest.java
@@ -3,16 +3,22 @@ package input;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputParserTest {
 
+    private static InputParser inputParser;
+    private static int count = 0;
+
     @Test
-    @DisplayName("When passed a single valid instruction of type String, returns array with single element of class Instruction matches user input.")
+    @DisplayName("When passed a single valid instruction of type String, returns Instruction to match user input.")
     void testParsesSingleValidInstruction() {
 
         //Arrange
-        InputParser inputParser = new InputParser();
+        inputParser = new InputParser();
 
         Instruction expectedLeft = Instruction.L;
         Instruction expectedRight = Instruction.R;
@@ -33,6 +39,42 @@ class InputParserTest {
                 () -> assertEquals(expectedRight, actualRight),
                 () -> assertEquals(expectedMove, actualMove)
         );
+    }
+
+    @Test
+    @DisplayName("When passed multiple consecutive valid instructions of type String, returns matching elements of class Instruction.")
+    void testParsesMultipleValidInstructions() {
+
+        //Arrange
+        inputParser = new InputParser();
+
+        List<Instruction> expectedFirstLog = List.of(Instruction.L,Instruction.R,Instruction.M);
+        List<Instruction> expectedSecondLog = List.of(Instruction.M,Instruction.L,Instruction.L,Instruction.R);
+        List<Instruction> expectedThirdLog = List.of(Instruction.R,Instruction.R,Instruction.M,Instruction.L,Instruction.L,Instruction.L,Instruction.M);
+
+        List<Instruction> actualFirstLog = new ArrayList<>();
+        List<Instruction> actualSecondLog = new ArrayList<>();
+        List<Instruction> actualThirdLog = new ArrayList<>();
+
+        //Act
+        inputParser.parseInstruction("LRM");
+        while (count < 3) actualFirstLog.add(getInstruction());
+        inputParser.parseInstruction("MLLR");
+        while (count < 7) actualSecondLog.add(getInstruction());
+        inputParser.parseInstruction("RRMLLLM");
+        while (count < 14) actualThirdLog.add(getInstruction());
+
+        // Assert
+        assertAll(
+                () -> assertEquals(expectedFirstLog, actualFirstLog),
+                () -> assertEquals(expectedSecondLog, actualSecondLog),
+                () -> assertEquals(expectedThirdLog, actualThirdLog)
+        );
+    }
+
+    private static Instruction getInstruction() {
+        count++;
+        return inputParser.getValidatedInstructions().poll();
     }
 
 }
