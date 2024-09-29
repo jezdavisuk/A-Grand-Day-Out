c3de2700d900395e1f468392525b45a4cde6e702
Jez <jezdavisuk@gmail.com>
2024-09-29 14:03:19 +0100

    feat: add class PlateauSize to package input demarcates arena boundaries

---
+++ src/main/java/input/PlateauSize.java
@@ -0,0 +1,20 @@
+package input;
+
+public class PlateauSize {
+
+    private final int xDim; // x dimension
+    private final int yDim; // y dimension
+
+    public PlateauSize(int xDim, int yDim) {
+        this.xDim = xDim;
+        this.yDim = yDim;
+    }
+
+    public int getxDim() {
+        return xDim;
+    }
+
+    public int getyDim() {
+        return yDim;
+    }
+}
